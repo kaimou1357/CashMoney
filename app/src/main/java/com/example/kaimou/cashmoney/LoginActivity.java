4f2fe6f497938199b006f7a5a348d4819c02cfd0
kaimou <kaimou1357>
2015-12-05 15:09:10 -0500

    Commit for Kevin

--- app/src/main/java/com/example/kaimou/cashmoney/LoginActivity.java
+++ app/src/main/java/com/example/kaimou/cashmoney/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.kaimou.cashmoney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,18 +15,20 @@ import android.view.MenuItem;
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = (Button) findViewById(R.id.loginButton);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         loginButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(startIntent);
+
             }
         });
 
---
+++ app/src/main/java/com/example/kaimou/cashmoney/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.kaimou.cashmoney;
+
+import android.app.Activity;
+
+/**
+ * Created by kaimou on 12/5/15.
+ */
+public class MainActivity extends Activity {
+}
