f16e7c6b3b7e98cf9e16a4a74b8ea9692051abb0
kaimou <kaimou1357>
2015-12-05 18:40:46 -0500

    Dialog NumberPicker

--- app/src/main/java/com/example/kaimou/cashmoney/LoginActivity.java
+++ app/src/main/java/com/example/kaimou/cashmoney/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.kaimou.cashmoney;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,24 +13,42 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
+    private TextView userEmail;
+    private TextView userPass;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userEmail = (TextView)findViewById(R.id.emailTextView);
+        userPass = (TextView) findViewById(R.id.passwordTextView);
+        prefs = getApplicationContext().getSharedPreferences("prefs", MODE_PRIVATE);
+        if (prefs.getBoolean("saveLogin", false)) {
+            userEmail.setText(prefs.getString("email", ""));
+            userPass.setText(prefs.getString("pass", ""));
+        }
         loginButton = (Button) findViewById(R.id.loginButton);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        loginButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences.Editor editPrefs = prefs.edit();
+                editPrefs.putBoolean("saveLogin", true);
+                editPrefs.putString("email", userEmail.getText().toString());
+                editPrefs.putString("pass", userPass.getText().toString());
+                editPrefs.apply();
                 Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 getApplicationContext().startActivity(startIntent);
             }
         });
+
 
 
     }
--- app/src/main/java/com/example/kaimou/cashmoney/MainActivity.java
+++ app/src/main/java/com/example/kaimou/cashmoney/MainActivity.java
@@ -1,28 +1,77 @@
 package com.example.kaimou.cashmoney;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 /**
  * Created by kaimou on 12/5/15.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements NumberPicker.OnValueChangeListener {
     private TextView loanAmount;
+    static Dialog d;
     private int maxAmountToLoan = 50; //Max amount to loan to this person. For test purposes, this is set to 50.
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
         loanAmount = (TextView) findViewById(R.id.make_a_loan_textview);
         loanAmount.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Bundle bundle = new Bundle();
-                bundle.putInt("MAX_LOAN_AMOUNT", maxAmountToLoan);
-                NumberPickerFragment npFragment = new NumberPickerFragment();
-                npFragment.setArguments(bundle);
+                showNumberPicker();
             }
         });
+    }
+
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+        //this is useless.
+
+    }
+
+    private void showNumberPicker(){
+        final Dialog d = new Dialog(MainActivity.this);
+        d.setTitle("Loan Amount");
+        d.setContentView(R.layout.loan_amount_numberpicker);
+        Button okButton = (Button)d.findViewById(R.id.button1);
+        Button cancelButton = (Button)d.findViewById(R.id.button2);
+        final NumberPicker np = (NumberPicker)d.findViewById(R.id.numberPicker1);
+        String[] test = {"1", "2" , "3"};
+
+        np.setMaxValue(maxAmountToLoan/5);
+        np.setMinValue(0);
+        np.setDisplayedValues(arrayGeneration(maxAmountToLoan));
+        np.setOnValueChangedListener(this);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                loanAmount.setText(String.valueOf(np.getValue()));
+                d.dismiss();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                d.dismiss();
+            }
+        });
+        d.show();
+
+
+    }
+
+    private String[] arrayGeneration(int amount){
+        String[] valuesLoaned = new String[amount/5];
+        int arrayCounter = 0;
+        //Amount to loan can only be in increments of 5.
+        for(int i = amount; i>0; i= i - 5){
+            valuesLoaned[arrayCounter] = String.valueOf(amount);
+            arrayCounter++;
+        }
+        return valuesLoaned;
+
     }
 
 
