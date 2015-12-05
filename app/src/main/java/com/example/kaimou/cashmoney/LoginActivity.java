82394d7a3fff49fe8326b2a9001e49e6f8947d7b
kaimou <kaimou1357>
2015-12-05 14:26:35 -0500

    loginactivity

---
+++ app/src/main/java/com/example/kaimou/cashmoney/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.kaimou.cashmoney;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                
+            }
+        });
+
+
+    }
+
+
+}
