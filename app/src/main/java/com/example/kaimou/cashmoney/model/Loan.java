0368c2830a4182050d956c47aef6ca7bd71f05cd
KevCron <kevin.john.cronly@gmail.com>
2015-12-05 22:05:35 -0500

    Loan refactor

--- app/src/main/java/com/example/kaimou/cashmoney/model/Loan.java
+++ app/src/main/java/com/example/kaimou/cashmoney/model/Loan.java
@@ -1,49 +1,145 @@
 
 package com.example.kaimou.cashmoney.model;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Loan {
 
-    private int amount;
+    private String Id;
     private String loanTo;
+    private int amount;
+    private int V;
+    private boolean paid;
+    private String deadline;
+    private String createdAt;
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     /**
-     * 
+     *
      * @return
-     *     The amount
+     * The Id
      */
-    public int getAmount() {
-        return amount;
+    public String getId() {
+        return Id;
     }
 
     /**
-     * 
-     * @param amount
-     *     The amount
+     *
+     * @param Id
+     * The _id
      */
-    public void setAmount(int amount) {
-        this.amount = amount;
+    public void setId(String Id) {
+        this.Id = Id;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The loanTo
+     * The loanTo
      */
     public String getLoanTo() {
         return loanTo;
     }
 
     /**
-     * 
+     *
      * @param loanTo
-     *     The loan_to
+     * The loan_to
      */
     public void setLoanTo(String loanTo) {
         this.loanTo = loanTo;
+    }
+
+    /**
+     *
+     * @return
+     * The amount
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     *
+     * @param amount
+     * The amount
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    /**
+     *
+     * @return
+     * The V
+     */
+    public int getV() {
+        return V;
+    }
+
+    /**
+     *
+     * @param V
+     * The __v
+     */
+    public void setV(int V) {
+        this.V = V;
+    }
+
+    /**
+     *
+     * @return
+     * The paid
+     */
+    public boolean isPaid() {
+        return paid;
+    }
+
+    /**
+     *
+     * @param paid
+     * The paid
+     */
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    /**
+     *
+     * @return
+     * The deadline
+     */
+    public String getDeadline() {
+        return deadline;
+    }
+
+    /**
+     *
+     * @param deadline
+     * The deadline
+     */
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    /**
+     *
+     * @return
+     * The createdAt
+     */
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     *
+     * @param createdAt
+     * The created_at
+     */
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
     }
 
     public Map<String, Object> getAdditionalProperties() {
